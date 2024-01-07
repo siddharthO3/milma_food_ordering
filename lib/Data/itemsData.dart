0e6546145aa14edee96ddb017e53a43bdfdff5ff
Aditya Sri Ram <aditya22bcs57@iiitkottayam.ac.in>
2024-01-07 23:20:26 +0530

    Images issue resolved

--- lib/Data/itemsData.dart
+++ lib/Data/itemsData.dart
@@ -2,15 +2,15 @@ import 'package:milma_food_ordering/Models/itemWidget.dart';
 
 DateTime currenttime = DateTime.now();
 final itemW = [
-  Item('assets/food1.jpg', 'Samosa',
+  Item('assets/image/food.jpg', 'Samosa',
       currenttime.add(const Duration(minutes: 10)), 4.5, 120, 20),
-  Item('assets/food1.jpg', 'Omlet',
+  Item('assets/image/food.jpg', 'Omlet',
       currenttime.add(const Duration(minutes: 30)), 4, 130, 40),
-  Item('assets/food1.jpg', 'Chole Bature',
+  Item('assets/image/food.jpg', 'Chole Bature',
       currenttime.add(const Duration(minutes: 60)), 3.5, 22, 80),
-  Item('assets/food1.jpg', 'Aloo Paratha',
+  Item('assets/image/food.jpg', 'Aloo Paratha',
       currenttime.add(const Duration(minutes: 45)), 4.8, 66, 40),
-  Item('assets/food1.jpg', 'MoMo', currenttime.add(const Duration(minutes: 30)),
+  Item('assets/image/food.jpg', 'MoMo', currenttime.add(const Duration(minutes: 30)),
       3.9, 91, 50),
 ];
 
